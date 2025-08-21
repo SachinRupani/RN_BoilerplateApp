f1c2092d050179805aa7f8a8c3ddcb90bcc61b81
Sachin Rupani <sachin.rupani1992@gmail.com>
2025-08-21 18:06:54 +0530

    Updated environment for staging branch

--- app/config/environment/SelectedEnvironment.ts
+++ app/config/environment/SelectedEnvironment.ts
@@ -1,7 +1,7 @@
-import {devEnvironment} from "./AppEnvironment";
+import {stagingEnvironment} from "./AppEnvironment";
 
 /**
  * --IMPORTANT--
  * selectedEnvironment should always point to the correct env object based on the branch we are merging to
  */
-export const selectedEnvironment = devEnvironment;
+export const selectedEnvironment = stagingEnvironment;
